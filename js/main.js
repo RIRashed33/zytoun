6157a9451935d25736d2429580928ff99a53293d
Rashed <ri.rashed33@gmail.com>
2024-12-29 13:58:06 +0600

    box section css done

---
+++ js/main.js
@@ -0,0 +1,14 @@
+(function($){
+    $(document).ready(function(){
+        $('#image_carousel').owlCarousel({
+            loop: true,
+            margin: 24,
+            nav: false,
+            dots: false,
+            autoWidth: true,
+            center:true,
+            startPosition: 2,
+        });
+
+    });
+}(jQuery));
